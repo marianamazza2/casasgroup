0979144dd23b01edd135f51902cd5016b9adb9a7
Mariana Mazzariello <marianamazzariello2@gmail.com>
2026-06-17 13:19:18 +0200

    update assets

--- src/routes/vender.tsx
+++ src/routes/vender.tsx
@@ -1,4 +1,4 @@
-import { createFileRoute, Link } from '@tanstack/react-router'
+import { createFileRoute } from '@tanstack/react-router'
 import { useEffect, useRef, useState } from 'react'
 import { motion, useScroll, useTransform } from 'framer-motion'
 import { Footer } from '../components/Footer'
